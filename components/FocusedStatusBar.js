4dd033719af70c1f1e0f24e76a83a4d94f85c094
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-29 16:52:30 -0600

    Update FocusedStatusBar.js

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
-import React from 'react'
 
-const FocusedStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusedStatusBar</Text>
-    </View>
-  )
+const FocusedStatusBar = (props) => {
+
+  const isFocused = useIsFocused()
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
 
 export default FocusedStatusBar
