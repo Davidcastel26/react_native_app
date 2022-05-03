50dccd1d1085f3b64cca53b8bd523d2a054f0f64
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-03 16:43:01 -0600

    Update SubInfo.js

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -43,7 +43,23 @@ export const People = () => {
 };
 
 export const EndDate = () => {
-  return <Text>hi</Text>;
+  return (
+    <View 
+      style={{
+        paddingHorizontal:SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent:'center',
+        alignItems:'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%'
+      }}
+      >
+        <Text style={{ fontFamily: FONTS.regular, fontSize: SIZES.small, color: COLORS.primary }}>Ending in</Text>
+        <Text style={{ fontFamily: FONTS.semiBold, fontSize: SIZES.medium, color: COLORS.primary }}> 12hr </Text>
+    </View>
+  )
 };
 
 export const SubInfo = () => {
