c8e6aaeb25a89cb935c21e560e4aedbe3f57891f
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-03 17:25:10 -0600

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -30,10 +30,19 @@ export const CircleButton = ({ imgUrl, handlePress, ...props}) => {
   )
 }
 
-export const RectButton = () => {
+export const RectButton = ({minWidth, fontSize, handlePress, ...props}) => {
   return (
-    <View>
-      <Text> hELLO </Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        padding: SIZES.small,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text style={{fontFamily: FONTS.semiBold, color: COLORS.white, fontSize: fontSize, textAlign: 'center' }}> Place a bid</Text>
+    </TouchableOpacity>
   )
 }
