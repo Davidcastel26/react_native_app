f4176a626c5cbb1317d7c0241140197943005aed
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-03 16:02:07 -0600

    Create SubInfo.js

---
+++ components/SubInfo.js
@@ -0,0 +1,41 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const NFTTitle = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  )
+}
+
+
+export const Eth = () => {
+  return (
+    <Text>hi</Text>
+  )
+}
+
+export const ImageCmp = () => {
+  return (
+    <Text>hi</Text>
+  )
+}
+
+export const People = () => {
+  return (
+    <Text>hi</Text>
+  )
+}
+
+export const EndDate = () => {
+  return (
+    <Text>hi</Text>
+  )
+}
+
+export const SubInfo = () => {
+  return (
+    <Text>hi</Text>
+  )
+}
