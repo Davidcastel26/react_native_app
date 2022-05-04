6a3d644f24eb6a7b72692778c181ad3055b01f87
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-04 10:01:56 -0600

    detail working

--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+
+import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../Constants'
 
 const Details = () => {
   return (
     <Text>
-      Details
+      
     </Text>
   )
 }
