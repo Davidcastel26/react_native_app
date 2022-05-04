5c247c6e5d708417ac06043b082f8983e20f29a9
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-04 10:19:48 -0600

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
 
 import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../Constants'
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDes, DetailsBid} from '../components'
 
 const Details = () => {
   return (
