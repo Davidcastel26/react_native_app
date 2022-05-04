4362b786589d1a7099ddf2ac7f7574701d827f5f
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-04 10:21:23 -0600

    Update index.js

--- components/index.js
+++ components/index.js
@@ -3,11 +3,15 @@ import NFTCard from "./NFTCard";
 import FocusedStatusBar from "./FocusedStatusBar";
 import {CircleButton, RectButton } from './Button'
 import {SubInfo, People, NFTTitle, EthPrice, EndDate, ImageCmp } from './SubInfo'
+import DetailsBid from './DetailsBid'
+import DetailsDes from "./DetailsDes";
 
 export {
   HomeHeader,
   NFTCard,
   FocusedStatusBar,
   CircleButton, RectButton,
-  SubInfo, People, NFTTitle, EndDate, EthPrice, ImageCmp
+  SubInfo, People, NFTTitle, EndDate, EthPrice, ImageCmp,
+  DetailsBid,
+  DetailsDes,
 }
