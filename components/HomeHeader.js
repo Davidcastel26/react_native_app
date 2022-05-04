729b00ebb5232edc687cefc395010d79f2260feb
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-03 19:05:30 -0600

    Update HomeHeader.js

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import { COLORS, FONTS, SIZES, assets } from '../Constants'
 
 const HomeHeader = () => {
   return (
     <View>
-      <Text>HomeHeader</Text>
+      <View style={{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+      }}>
+         <Image 
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 25}}
+         />
+         <View style={{width: 45, height: 45}}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{width:'100%', height:'100%'}}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{width:15, height:15, position:'absolute', bottom:0, right:0 }}
+          />
+         </View>
+      </View>
+      <View 
+        style={{ marginVertical: SIZES.font}}
+      >
+        <Text
+          style={{fontFamily:FONTS.regular, fontSize: SIZES.small, color: COLORS.white}}
+        >
+          Hello Dave
+        </Text>
+      </View>
     </View>
   )
 }
