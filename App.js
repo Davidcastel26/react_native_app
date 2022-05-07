0a0b1c8f0a07d36ea427808a36c11161386fd58a
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-07 16:50:39 -0600

    update app.js

--- App.js
+++ App.js
@@ -9,23 +9,29 @@ const Stack = createStackNavigator();
 
 const theme = {
   ...DefaultTheme,
-  colors:{
+  colors: {
     ...DefaultTheme.colors,
-    background: "transparent"
-  }
-}
+    background: "transparent",
+  },
+};
 
 const App = () => {
 
   const [loaded] = useFonts({
-    InterBold: require("./assets/fonts/Inter-Bold.ttf")
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
   })
+
+  if( !loaded ) return null;
 
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home}/>
-        <Stack.Screen  name='Details' component={Details}/>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen  name="Details" component={Details}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/Details.js
+++ screens/Details.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
 
 import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../Constants'
