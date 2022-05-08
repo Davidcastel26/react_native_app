e29dba455efaa46a893a4fbfffe6111edd442abc
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-07 18:10:06 -0600

    Update Details.js

--- screens/Details.js
+++ screens/Details.js
@@ -1,38 +1,83 @@
-import React from 'react'
-import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
-import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../Constants'
-import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDes, DetailsBid} from '../components'
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../Constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDes,
+  DetailsBid,
+} from "../components";
 
-const Details = ({ route, navigation}) => {
-  
+const DetailsHeader = ({data, navigation}) => (
+  <View 
+    style={{
+      width:'100%',
+      height: 373
+    }}
+  >
+    <Image
+      source={data.Image}
+      resizeMode="cover"
+      style={{ width: '100%', height: '100%'}}
+    />
+
+    <CircleButton 
+      imgUrl={assets.left}
+      handlePress={()=>navigation.goBack()}
+    />
+  </View>
+)
+
+const Details = ({ route, navigation }) => {
   // console.log(route);
   const { data } = route.params;
 
   return (
-    <SafeAreaView style={{flex: 1}} >
+    <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar
         barStyle="dark-content"
         backgroundColor="transparent"
-        transLucent={true}
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          zIndex: 1,
+        }}
       >
-        <View 
-          style={{
-            width: '100%',
-            position:'absolute',
-            bottom:0,
-            paddingVertical:SIZES.font,
-            justifyContent:'center',
-            alignItems:'center',
-            backgroundColor:'rgba(255, 255, 255, 0.5)',
-            zIndex: 1
-          }}
-        >
-          <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark}/>
-        </View>
-      </FocusedStatusBar>
-    </SafeAreaView>
-  )
-}
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
 
-export default Details 
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3}}
+        ListHeaderComponent={()=>(
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Details;
