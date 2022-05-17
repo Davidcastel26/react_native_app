87e9aa5a3f22daf701e0c41b77d2b4368150a283
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-17 16:46:24 -0600

    Update DetailsDes.js

--- components/DetailsDes.js
+++ components/DetailsDes.js
@@ -5,6 +5,10 @@ import { EthPrice, NFTTitle } from './SubInfo'
 import { COLORS, SIZES, FONTS } from '../Constants'
 
 const DetailsDes = ({ data }) => {
+
+  const [text, setText] = useState(data.description.slice(0,100))
+  const [readMore, setReadMore] = useState(false)
+
   return (
    <>
     <View style={{
@@ -21,6 +25,27 @@ const DetailsDes = ({ data }) => {
         subTitleSize={SIZES.font}
       />
       <EthPrice price={data.price}/>
+    </View>
+
+    <View style={{ marginVertical:SIZES.extraLarge * 1.5}}>
+      <Text style={{ 
+        fontSize: SIZES.font, 
+        fontWeight:'600',
+        color: COLORS.primary
+        }}>
+        Description
+      </Text>
+      <View style={{ marginTop: SIZES.base}}>
+      <Text style={{ 
+        fontSize: SIZES.small, 
+        fontWeight:'400',
+        color: COLORS.secondary,
+        lineHeight: SIZES.large
+        }}>
+          {text}
+        {!readMore && '...'}
+        </Text>
+      </View>
     </View>
    </>
   )
